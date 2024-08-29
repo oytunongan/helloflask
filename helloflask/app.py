cb7d6fda97ecee9c5c3c2e6210c4cf8aacf5aa2a
Oytun <140172465+oytunongan@users.noreply.github.com>
2024-08-29 17:41:01 +0300

    Add files via upload

---
+++ helloflask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+COLORS = [
+    "red",
+    "blue"
+]
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method=="GET":
+        return render_template("index.html", color=COLORS)
+    else:
+        print("Form submitted!")
+        color = request.form.get("color")
+        if color in COLORS:
+            return render_template("color.html", color=color)
+        else:
+            return render_template("color.html", color="Not valid")
